c2e9892fcdfa93d016222adb5ab34ad88738e849
Unknown <m68476521.1@gmail.com>
2018-05-19 22:29:36 -0700

    feat: buttons formatted

--- app/src/main/java/com/m68476521/mike/kalculator/MainActivity.kt
+++ app/src/main/java/com/m68476521/mike/kalculator/MainActivity.kt
@@ -2,11 +2,18 @@ package com.m68476521.mike.kalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonNumberEvent(view: View) {
+        val viewSelected = view as Button
+
     }
 }
